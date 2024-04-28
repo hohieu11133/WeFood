15c857f27f3c8d923ceb242d608fc54a77c8b535
hohieu1111 <130475053+hohieu1111@users.noreply.github.com>
2024-04-29 02:29:01 +0700

    getallpost

--- app/src/main/java/com/ct07/wefood/MainActivity.kt
+++ app/src/main/java/com/ct07/wefood/MainActivity.kt
@@ -3,16 +3,23 @@ package com.ct07.wefood
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ct07.wefood.newfeed.model.Post
+import com.ct07.wefood.newfeed.view.NewFeed
+import com.ct07.wefood.newfeed.viewmodel.PostViewModel
 import com.ct07.wefood.ui.theme.WeFoodTheme
 
 class MainActivity : ComponentActivity() {
+    val postviewModel by viewModels<PostViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,22 +29,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    postList(postList = postviewModel.postListResponse.value)
+                    postviewModel.getPostList()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WeFoodTheme {
-        Greeting("Android")
+fun postList(postList: List<Post>){
+    LazyColumn{
+        itemsIndexed(postList){index, post ->
+            NewFeed(post = post)
+            post
+        }
     }
+
 }
